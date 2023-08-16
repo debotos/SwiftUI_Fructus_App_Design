7756c6bd97c49db664c8e6f0b33f68654dff6a9c
Debotos Das <debotosdas@gmail.com>
2023-08-16 08:02:30 +0600

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Personal on 16/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
